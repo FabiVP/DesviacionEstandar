b564e446637118e748c04ac92cd6ba183d5a13d2
72947169 <72947169@continentl.edu.pe>
2024-10-21 21:14:01 -0500

    Caso 4: Calcular promedio con un  n elementos

--- tests/TestDesviacionEstandar_Prom.py
+++ tests/TestDesviacionEstandar_Prom.py
@@ -13,3 +13,7 @@ class TestDesviacionEstandar_Prom(unittest.TestCase):
     def test_dosElementos_retornaPromedioElementos(self):
         elementos = DesviacionEstandar_Prom([15, 17])
         self.assertEqual(16, elementos.promedio())
+
+    def test_nElementos_retornaPromedioNElementos(self):
+        elementos = DesviacionEstandar_Prom([12, 14, 18, 19, 10, 15])
+        self.assertEqual((12 + 14 + 18 + 19 + 10 + 15) / 6, elementos.promedio())
