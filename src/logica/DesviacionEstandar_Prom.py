5061426ebfe0b13411b60dae550b85e68c324ff3
72947169 <72947169@continentl.edu.pe>
2024-10-21 20:49:14 -0500

    Creando el esquelto de las clases desviacion Estandar/ Promedio y Test Desviacion/Promedio

--- src/logica/DesviacionEstandar_Prom.py
+++ src/logica/DesviacionEstandar_Prom.py
@@ -4,6 +4,12 @@ class DesviacionEstandar:
     def __init__(self, desviacionE):
         self.__desviacionE = desviacionE
 
+    def promedio(self):
+        if len(self.__desviacionE) > 0:
+            return sum(self.__desviacionE) / len(self.__desviacionE)
+        else:
+            return None
+
     def calcular(self):
         if len(self.__desviacionE) > 1:
             media = sum(self.__desviacionE) / len(self.__desviacionE)
@@ -11,3 +17,6 @@ class DesviacionEstandar:
             return math.sqrt(varianza)
         else:
             return None
+
+
+
---
+++ tests/TestDesviacionEstandar_Prom.py
@@ -0,0 +1,8 @@
+import unittest
+from src.logica.DesviacionEstandar import DesviacionEstandar
+
+class TestDesviacionEstandar(unittest.TestCase):
+
+    
+
+
