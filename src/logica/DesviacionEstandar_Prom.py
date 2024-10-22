7ab32582adc6ddb31b53de182ee4c568ac26d127
72947169 <72947169@continentl.edu.pe>
2024-10-21 21:00:35 -0500

    Caso 1: Calcular promedio con conjunto vacío

--- src/logica/DesviacionEstandar_Prom.py
+++ src/logica/DesviacionEstandar_Prom.py
@@ -1,19 +1,19 @@
 import math
 
-class DesviacionEstandar:
-    def __init__(self, desviacionE):
-        self.__desviacionE = desviacionE
+class DesviacionEstandar_Prom:
+    def __init__(self, numeros):
+        self.__numeros = numeros
 
     def promedio(self):
-        if len(self.__desviacionE) > 0:
-            return sum(self.__desviacionE) / len(self.__desviacionE)
+        if len(self.__numeros) > 0:
+            return sum(self.__numeros) / len(self.__numeros)
         else:
             return None
 
     def calcular(self):
-        if len(self.__desviacionE) > 1:
-            media = sum(self.__desviacionE) / len(self.__desviacionE)
-            varianza = sum((x - media) ** 2 for x in self.__desviacionE) / len(self.__desviacionE)
+        if len(self.__numeros) > 1:
+            media = sum(self.__numeros) / len(self.__numeros)
+            varianza = sum((x - media) ** 2 for x in self.__numeros) / len(self.__numeros)
             return math.sqrt(varianza)
         else:
             return None
--- tests/TestDesviacionEstandar_Prom.py
+++ tests/TestDesviacionEstandar_Prom.py
@@ -1,8 +1,10 @@
 import unittest
-from src.logica.DesviacionEstandar import DesviacionEstandar
+from src.logica.DesviacionEstandar_Prom import DesviacionEstandar_Prom
 
-class TestDesviacionEstandar(unittest.TestCase):
+class TestDesviacionEstandar_Prom(unittest.TestCase):
+    def test_sin_elementos(self):
+        elementos = DesviacionEstandar_Prom([])
+        self.assertIsNone(elementos.promedio())
 
-    
 
 
